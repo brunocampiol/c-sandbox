bf3903c52b99ce4afbd2ba062288f278b8a2931a
bruno <bruno.campiol@gmail.com>
2024-01-04 17:37:56 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# c-sandbox
-c code sandbox
+# C Code Sandbox
+
+## C in Windows
+
+### MinGW
+
+Install via chocolatey MinGW-w64
+
+C:\ProgramData\mingw64\mingw64\bin
+
+gcc.exe
+
+Once installed, gcc in terminal should recognize "gcc -v"
+
+### VS Code
+
+Extensions:
+* C/C++ Microsoft
+* C/C++ Compile Run - danielpinto8zz6
+		
+Shortcuts:
+* F5 start debug
+* F7
+	
+https://www.geeksforgeeks.org/multiply-large-numbers-represented-as-strings/
+
+18446744073709551615
+
+99999999999999999999
+
+1235421415454545454545454544
+
+1714546546546545454544548544544545
---
+++ src/is-prime.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <time.h>
+#include <unistd.h>
+
+clock_t start;
+
+bool IsPrime(unsigned long long int n)
+{
+    if (n == 2 || n == 3)
+        return true;
+
+    if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (int i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    printf("Starting to find prime numbers\n");
+    start = clock();
+
+    // 18,446,744,073,709,551,615 - 50,000,000,000,000,000,000
+    // 50000000000000000000 => 0xB1A2BC2EC50000ull
+    // for (unsigned long long int i = INT_MAX; i < ULLONG_MAX; i++)
+
+    for (unsigned long long int i = 0xB1A2BC2EC50000ull; i < ULLONG_MAX; i++)
+    {
+        if (IsPrime(i))
+        {
+            clock_t end = clock();
+            double elapsed = (double)(end - start) / (double)CLOCKS_PER_SEC;
+            printf("%llu is prime - Elapsed time %.2f\n", i, elapsed);
+            // sleep(1);
+        }
+    }
+
+    return 0;
+}
